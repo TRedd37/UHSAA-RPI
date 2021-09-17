3de87424d795c4f66e2af63fcbb866199a00a7e1
Taylor Redd <tredd37@gmail.com>
2021-09-17 09:01:10 -0600

    Update main.R

--- main.R
+++ main.R
@@ -17,49 +17,54 @@ full_schedule <- team_info %>%
   as.data.frame() %>%
   pmap_dfr(getTeamSchedule)
 
+finished_games <- "https://docs.google.com/spreadsheets/d/1Qfa8i306cl47qistk-3D9NEapFn5XLG-8QGn006ySJ4/edit#gid=229011846" %>%
+  read_sheet(sheet = "Finished Games") %>%
+  filter(!is.na(Winner)) %>%
+  select(-c(Date, Lock)) %>%
+  createSenarios()
+
 scenarios <- "https://docs.google.com/spreadsheets/d/1Qfa8i306cl47qistk-3D9NEapFn5XLG-8QGn006ySJ4/edit#gid=229011846" %>%
-  read_sheet(sheet = "Scenario Builder") %>%
+  read_sheet(sheet = "All Remaining") %>%
+  filter(!is.na(Winner)) %>%
+  select(-c(Date, Lock)) %>%
   createSenarios()
   
+# test_schedule <- full_schedule %>%
+#   mutate(Date = as.Date(Date)) %>%
+#   filter(Date < as.Date("2021-05-04")) %>%
+#   mutate(OwnScore = ifelse((Team == "Ridgeline" & 
+#                             Opponent =="Logan" & 
+#                             Date == "2021-04-30"), 50, OwnScore)) %>%
+#   mutate(OwnScore = ifelse((Opponent == "Ridgeline" & 
+#                             Team =="Logan" & 
+#                               Date == "2021-04-30"), 0, OwnScore))
 
-test_schedule <- full_schedule %>%
-  mutate(Date = as.Date(Date)) %>%
-  filter(Date < as.Date("2021-05-04")) %>%
-  mutate(OwnScore = ifelse((Team == "Ridgeline" & 
-                            Opponent =="Logan" & 
-                            Date == "2021-04-30"), 50, OwnScore)) %>%
-  mutate(OwnScore = ifelse((Opponent == "Ridgeline" & 
-                            Team =="Logan" & 
-                              Date == "2021-04-30"), 0, OwnScore))
-
-
+full_schedule <- full_schedule %>%
+  filter(!is.na(OwnScore))
 
 test_schedule <- full_schedule %>%
   filter(!is.na(OwnScore)) %>%
-  rbind(scenario)
+  rbind(scenarios)
 
-test_schedule <- full_schedule %>%
-  filter(!is.na(OwnScore))
-
-         
 teams <- test_schedule %>%
   pull(Team) %>%
   unique()
 
 RPIs <- data.frame(team_name = teams) %>%
-  pmap_dfr(calculateRPI, schedule = test_schedule) %>%
+  pmap_dfr(calculateRPI, schedule = full_schedule) %>%
   mutate(Team = teams) %>%
-  relocate(Team) %>%
-  arrange(desc(RPI))
+  arrange(desc(RPI)) %>%
+  mutate(RPI_Rank = row_number()) %>%
+  relocate(RPI_Rank, Team, RPI) 
 
 RPIs %>%
   sheet_write("https://docs.google.com/spreadsheets/d/1Qfa8i306cl47qistk-3D9NEapFn5XLG-8QGn006ySJ4/edit#gid=229011846",
-              sheet = "TaylorRPI")
+              sheet = "RPI")
 
 calculateRPI("Alta", test_schedule)
 calculateWP("Bountiful", test_schedule)
 calculateIndividualOWP("Alta", "Timpview", full_schedule)
-team_name <- "American Fork"
+team_name <- "Olympus"
 
 full_schedule %>%
   filter(Team == team_name) %>%
@@ -67,3 +72,7 @@ full_schedule %>%
   select(team_name, opponent) %>%
   pmap_dfr(calculateOWP, schedule = test_schedule) %>%
   summarize(OWP = mean(WP))
+
+full_schedule %>%
+  filter(is.na(OwnScore)) %>% 
+  nrow()
